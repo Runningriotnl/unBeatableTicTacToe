7dee44f237417699e2b0380b88d8381c25faf484
Runningriotnl <prkorver@hotmail.com>
2020-01-10 14:06:35 +0100

    Added Thymeleaf to pom and made a simple example page.

---
+++ src/main/java/com/tictactoe/game/GameController.java
@@ -0,0 +1,15 @@
+package com.tictactoe.game;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GameController
+{
+    @GetMapping("/game")
+    public String greeting(Model model) {
+        return "game";
+    }
+}
